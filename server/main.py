18c27c5aba3a2d0b1e3f2afbbd8142a6928d2f2a
KIM GYEOUNG HO <67896210+vwaiolet@users.noreply.github.com>
2023-07-18 16:32:39 +0900

    Updata server

--- server/main.py
+++ server/main.py
@@ -5,19 +5,50 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
+# Download Image -> AI Model Predict -> Delete Image
+# POST /predict
+# Request JSON
+'''
+    {
+        "url" : "Image URL"
+    }
+'''
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     url = data['url']
+    slash = url.rfind('/')
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        # 이미지 저장
-        with open('temp/image.png', 'wb') as f:
-            f.write(response.content)
-        return Response(status=201, mimetype='application/json')
+    # File Name Parsing
+    # at URL, split after find '/'
+    if slash != -1:
+        file_name = url[slash + 1:]
     else:
-        return Response(status=400, mimetype='application/json')
+        file_name = 'image.png'
+
+    # Verify Code Parsing
+    # at File Name, split between '_' and '.'
+    # Random 6 character
+    # Using delete file
+    vc_ub = file_name.rfind('_')
+    vc_dot = file_name.rfind('.')
+    vc = file_name[vc_ub:vc_dot]
+    print(vc)
+
+    # Custom User-Agent
+    cua = 'vwaiolet'
+    params = {'useragent': cua}
+
+    # address?useragent=vwaiolet
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        # Download Image
+        # Directory is temp/file_name
+        with open('temp/' + file_name, 'wb') as f:
+            f.write(response.content)
+        return Response(status=201)
+    else:
+        return Response(status=400)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)

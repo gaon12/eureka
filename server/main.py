487de6460ba855034f6bd3dbc1e8f58c1b3266c4
zionail <93862569+zionails@users.noreply.github.com>
2023-07-19 15:47:05 +0900

    Add files via upload

--- server/main.py
+++ server/main.py
@@ -5,39 +5,62 @@ from detect import LicensePlateRecognizer
 import os
 import json
 
+# Download Image -> AI Model Predict -> Delete Image
+# POST /predict
+# GET /predict -> delete request
+# Request JSON
+'''
+    {
+        "url" : "Image URL"
+    }
+'''
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    data = request.get_json()
-    url = data['url']
+    if request.method == 'POST':
+        # Custom User-Agent
+        data = request.get_json()
+        url = data['url']
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        # 이미지 저장
-        with open('temp/image.png', 'wb') as f:
-            f.write(response.content)
+        # Custom User-Agent
+        # cua = 'vwaiolet'
+        # params = {'useragent': cua}
 
-        # 이미지 인식을 위한 클래스 인스턴스 생성
-        recognizer = LicensePlateRecognizer(model_path='Model/best.pt')
+        # address?useragent=vwaiolet
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Download Image
+            # Directory is temp/file_name
+            with open('temp/image.png', 'wb') as f:
+                f.write(response.content)
 
-        # 이미지 인식 및 결과 반환
-        result = recognizer.recognize_license_plates('temp/image.png')
+            # 이미지 인식을 위한 클래스 인스턴스 생성
+            recognizer = LicensePlateRecognizer(model_path='Model/best.pt')
 
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": result
-        }
-        json_response = json.dumps(response_data)
+            # 이미지 인식 및 결과 반환
+            result = recognizer.recognize_license_plates('temp/image.png')
 
-        # 결과를 JSON 형식으로 반환
-        return Response(json_response, status=201, mimetype='application/json')
-    else:
-        return Response(status=400, mimetype='application/json')
+            # 결과를 JSON 형식으로 변환
+            response_data = {
+                "result": "success"
+            }
+            json_response = json.dumps(response_data)
 
+            # 결과를 JSON 형식으로 반환
+            return Response(json_response, status=201, mimetype='application/json')
+        else:
+            return Response(status=400, mimetype='application/json')
+    elif request.method == 'GET':
+        return "GET request received"
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def db
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

09346bede732b81b5abfe0c5a163b161f6ea9a4c
KIM GYEOUNG HO <67896210+vwaiolet@users.noreply.github.com>
2023-07-24 15:28:22 +0900

    Update main.py

--- server/main.py
+++ server/main.py
@@ -4,20 +4,11 @@ import requests
 from detect import LicensePlateRecognizer
 import os
 import json
-from flask_sqlalchemy import SQLAlchemy
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 app = Flask(__name__)
 CORS(app)
-
-# 데이터베이스 연결 설정
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/db_name'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# SQLAlchemy 객체 생성
-db = SQLAlchemy(app)
-
 
 # Download Image -> AI Model Predict -> Delete Image
 # POST /predict
@@ -78,70 +69,6 @@ def predict():
             return Response(json_response, status=201, mimetype='application/json')
         else:
             return Response(status=400, mimetype='application/json')
-
-
-@app.route('/user', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def user():
-    if request.method == 'POST':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=201, mimetype='application/json')
-    elif request.method == 'GET':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
-    elif request.method == 'PUT':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
-    elif request.method == 'DELETE':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
-
-
-@app.route('/car', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def car():
-    if request.method == 'POST':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=201, mimetype='application/json')
-    elif request.method == 'GET':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
-    elif request.method == 'PUT':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
-    elif request.method == 'DELETE':
-        # 결과를 JSON 형식으로 변환
-        response_data = {
-            "result": "success"
-        }
-        json_response = json.dumps(response_data)
-        return Response(json_response, status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':

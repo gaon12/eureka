8e7f93e59ef9b34a4d989161e64748582c75e617
zionail <93862569+zionails@users.noreply.github.com>
2023-08-07 14:38:53 +0900

    Add files via upload

--- server/main.py
+++ server/main.py
@@ -4,11 +4,13 @@ import requests
 from detect import LicensePlateRecognizer
 import os
 import json
+import re
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 app = Flask(__name__)
 CORS(app)
+
 
 # Download Image -> AI Model Predict -> Delete Image
 # POST /predict
@@ -16,10 +18,13 @@ CORS(app)
 # Request JSON
 '''
     {
-        "url" : "Image URL"
+        "url" : "Image URL",
+        "verify_code" : ""
     }
 '''
-@app.route('/predict', methods=['POST'])
+
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
         data = request.get_json()
@@ -61,15 +66,45 @@ def predict():
 
             # 결과를 JSON 형식으로 변환
             response_data = {
-                "result": "success"
+                "result": "success",
+                "car_number": "1234",
+                "is_regist": "Y/N",
+                "is_guest": "(Y/N)/null",
+                "is_electric": "(Y/N)/null",
+                "is_disabled": "(Y/N)/null",
             }
             json_response = json.dumps(response_data)
 
-            # 결과를 JSON 형식으로 반환
             return Response(json_response, status=201, mimetype='application/json')
         else:
             return Response(status=400, mimetype='application/json')
 
+    elif request.method == 'GET':
+        data = request.get_json()
+        url = data['url']
+
+        # URL에서 "username"과 "code" 추출
+        pattern = r"https://api\.eureka\.uiharu\.dev/img/([^_]+)_([^_]+)\.(\w+)"
+        matches = re.match(pattern, url)
+
+        if matches:
+            username = matches.group(1)
+            code = matches.group(2)
+            extension = matches.group(3)
+
+            # 추출한 "username"과 "code"를 사용하여 GET 요청 보내기
+            base_url = "https://api.eureka.uiharu.dev/img.php"
+            params = {
+                "filename": f"{username}_{code}.{extension}",
+                "verify_code": code,
+                "user-agent": "testua"
+            }
+
+            response = requests.get(base_url, params=params)
+            print(response.text)
+        else:
+            print("No match found.")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=9035)

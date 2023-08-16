56d410acd381b3e6ffb574d9ccf49afca20e4d7b
zionail <93862569+zionails@users.noreply.github.com>
2023-08-16 14:42:40 +0900

    Add files via upload

--- server/main.py
+++ server/main.py
@@ -5,16 +5,23 @@ from detect import LicensePlateRecognizer
 import os
 import json
 import re
-from db_connector import db, Car
+from werkzeug.utils import secure_filename
+
+# from db_connector import db, Car
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 app = Flask(__name__)
 CORS(app)
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://guest1:0YYL!i[-}F)UTkt8G@apt-manager.mysql.database.azure.com/car'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+
+# img 폴더 경로 설정
+UPLOAD_FOLDER = os.path.join(app.root_path, 'temp')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# app.config[
+#     'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://guest1:0YYL!i[-}F)UTkt8G@apt-manager.mysql.database.azure.com/car'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
 
 # Download Image -> AI Model Predict -> Delete Image
 # POST /predict
@@ -28,61 +35,68 @@ db.init_app(app)
 '''
 
 
+# send_request 함수
+def send_request(data_to_send, retries=3):
+    nodejs_server_url = "http://your-nodejs-server-url.com/api/data"
+
+    for _ in range(retries):
+        try:
+            response = requests.post(nodejs_server_url, json=data_to_send)
+            response_data = response.json()
+
+            if response.status_code == 200:
+                return response_data
+        except requests.exceptions.RequestException:
+            pass
+
+    return None
+
+
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        data = request.get_json()
-        url = data['url']
-        slash = url.rfind('/')
-
-        # File Name Parsing
-        # at URL, split after find '/'
-        if slash != -1:
-            file_name = url[slash + 1:]
-        else:
-            file_name = 'image.png'
-
-        # Verify Code Parsing
-        # at File Name, split between '_' and '.'
-        # Random 6 character
-        # Using delete file
-        vc_ub = file_name.rfind('_')
-        vc_dot = file_name.rfind('.')
-        vc = file_name[vc_ub:vc_dot]
-
+        f = request.files['file']
+        # img 폴더에 저장하기 위한 경로 생성
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+        f.save(upload_path)
         # Custom User-Agent
         cua = 'vwaiolet'
         params = {'useragent': cua}
 
+        # 이미지 인식을 위한 클래스 인스턴스 생성
+        recognizer = LicensePlateRecognizer(model_path='Model/best.pt')
+
+        # 이미지 인식 및 결과 반환
+        result = recognizer.recognize_license_plates('temp/' + f.filename)
+
         # address?useragent=vwaiolet
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            # Download Image
-            # Directory is temp/file_name
-            with open('temp/' + file_name, 'wb') as f:
-                f.write(response.content)
+        # response = requests.get(url, params=params)
 
-            # 이미지 인식을 위한 클래스 인스턴스 생성
-            recognizer = LicensePlateRecognizer(model_path='Model/best.pt')
+        nodejs_server_url = "http://118.67.130.191:3000/car/info"  # Node.js 서버의 API 엔드포인트
+        data_to_send = {
+            "car_number": result
+        }
 
-            # 이미지 인식 및 결과 반환
-            result = recognizer.recognize_license_plates('temp/' + file_name)
+        # send_request 함수를 통해 요청 보내고 결과 받기
+        response_data = send_request(data_to_send)
+        # 결과를 JSON 형식으로 변환
+        # response_data = {
+        #     "result": "success",
+        #     "car_number": "1234",
+        #     "is_regist": "Y/N",
+        #     "is_guest": "(Y/N)/null",
+        #     "is_electric": "(Y/N)/null",
+        #     "is_disabled": "(Y/N)/null"
+        # }
+        try:
+            os.remove(upload_path)
+            print("File deleted successfully.")
+        except OSError as e:
+            print("Error:", e)
 
-            # DB에서 "car_number"로 조회하여 결과 가져오기
-            data_from_db = Car.query.filter_by(car_number=result).first()
-
-            # 결과를 JSON 형식으로 변환
-            # response_data = {
-            #     "result": "success",
-            #     "car_number": "1234",
-            #     "is_regist": "Y/N",
-            #     "is_guest": "(Y/N)/null",
-            #     "is_electric": "(Y/N)/null",
-            #     "is_disabled": "(Y/N)/null"
-            # }
-
+        if response_data is not None:
             if result:
-                if data_from_db.is_regist == 0:
+                if response_data['is_regist'] == 0:
                     response_data = {
                         "result": "success",
                         "car_number": result,
@@ -92,10 +106,9 @@ def predict():
                         "is_disabled": None
                     }
                 else:
-                    is_guest = 'O' if data_from_db.is_guest == 1 else 'X'
-                    is_electric = 'O' if data_from_db.is_electric == 1 else 'X'
-                    is_disabled = 'O' if data_from_db.is_disabled == 1 else 'X'
-
+                    is_guest = 'O' if response_data['is_guest'] == 1 else 'X'
+                    is_electric = 'O' if response_data['is_electric'] == 1 else 'X'
+                    is_disabled = 'O' if response_data['is_disabled'] == 1 else 'X'
                     response_data = {
                         "result": "success",
                         "car_number": result,
@@ -104,42 +117,17 @@ def predict():
                         "is_electric": is_electric,
                         "is_disabled": is_disabled
                     }
+
+                json_response = json.dumps(response_data)
+                return Response(json_response, status=201, mimetype='application/json')
             else:
-                response_data = {
-                    "result": "false"
-                }
+                response_data = {"result": "false"}
+                json_response = json.dumps(response_data)
+                return Response(json_response, status=400, mimetype='application/json')
 
-            json_response = json.dumps(response_data)
-
-            return Response(json_response, status=201, mimetype='application/json')
         else:
+            # Node.js 서버 응답이 실패한 경우
             return Response(status=400, mimetype='application/json')
-
-    elif request.method == 'GET':
-        data = request.get_json()
-        url = data['url']
-
-        # URL에서 "username"과 "code" 추출
-        pattern = r"https://api\.eureka\.uiharu\.dev/img/([^_]+)_([^_]+)\.(\w+)"
-        matches = re.match(pattern, url)
-
-        if matches:
-            username = matches.group(1)
-            code = matches.group(2)
-            extension = matches.group(3)
-
-            # 추출한 "username"과 "code"를 사용하여 GET 요청 보내기
-            base_url = "https://api.eureka.uiharu.dev/img.php"
-            params = {
-                "filename": f"{username}_{code}.{extension}",
-                "verify_code": code,
-                "user-agent": "testua"
-            }
-
-            response = requests.get(base_url, params=params)
-            print(response.text)
-        else:
-            print("No match found.")
 
 
 if __name__ == '__main__':

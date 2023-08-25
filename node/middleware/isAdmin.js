0d9a0556bd8c57782a0187200d805dd2c77119ce
KIM GYEOUNG HO <67896210+vwaiolet@users.noreply.github.com>
2023-08-25 14:11:31 +0900

    관리자 판별 로직 개선

--- node/middleware/isAdmin.js
+++ node/middleware/isAdmin.js
@@ -1,10 +1,22 @@
-exports.isAdmin = (req, res, next) => {
+const db = require('../lib/db');
+
+exports.isAdmin = async (req, res, next) => {
     const nickname = req.session.nickname.split('-');
+    const dong = nickname[0];
     const ho = nickname[1];
 
-    if (ho === 1) {
+    const userSearch = await db.query('SELECT isAdmin FROM user WHERE dong = ? AND ho = ?', [dong, ho]);
+    const isAdmin = userSearch[0][0].isAdmin;
+
+    if (isAdmin == 1) {
         next();
     } else {
-        res.status(403).json({ error: 'Forbidden' });
+        res.status(403).json({
+            "status": 403,
+            "error": {
+                "errorCode": "E420",
+                "message": "관리자 권한 필요"
+            }
+        });
     }
 }

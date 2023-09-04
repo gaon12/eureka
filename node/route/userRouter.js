274a42d1c88a9a6743b7ee7ef91deebb1b326d07
KIM GYEOUNG HO <67896210+vwaiolet@users.noreply.github.com>
2023-09-04 14:34:01 +0900

    중복 로그인 로직 추가

--- node/route/userRouter.js
+++ node/route/userRouter.js
@@ -85,6 +85,16 @@ router.post('/signin', isSignout, async (req, res, next) => {
                 const isPwMatch = await bcrypt.compare(pw, existUser[0].pw);
 
                 if (isPwMatch) {
+                    if (req.session.is_logined) {
+                        return res.json({
+                            "status": 400,
+                            "error": {
+                                "errorCode": "E406",
+                                "message": "이미 로그인 된 사용자"
+                            }
+                        })
+                    }
+
                     req.session.is_logined = true;
                     req.session.nickname = dong + '-' + ho;
                     await new Promise((resolve, reject) => {

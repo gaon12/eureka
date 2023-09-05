6a8051e963d98e06f31397e3cb252344e43f3378
gaon12 <34927797+gaon12@users.noreply.github.com>
2023-09-05 17:35:09 +0900

    대한민국 정책브리핑 RSS로 뉴스 가져옴

--- junho/user/Main.js
+++ junho/user/Main.js
@@ -1,11 +1,7 @@
-import React from "react";
-import {
-  Table,
-  Typography,
-  Divider,
-  Card,
-  Layout
-} from "antd";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { parseString } from "xml2js";
+import { Table, Typography, Divider, Card, Layout, Button } from "antd";
 import NavBar from "./navbar";
 
 const { Title } = Typography;
@@ -15,77 +11,102 @@ function createNotice(name) {
   return { name };
 }
 
-function createNews(title) {
-  return { title };
-}
-
-const rows = [
-  createNotice("공지사항1"),
-  createNotice("공지사항2"),
-  createNotice("공지사항3"),
-  createNotice("공지사항4"),
-  createNotice("공지사항5")
-].slice(0, 6);
-
-const news = [
-  createNews("뉴스1"),
-  createNews("뉴스2"),
-  createNews("뉴스3"),
-  createNews("뉴스4"),
-  createNews("뉴스5")
-].slice(0, 6);
-
 function Main() {
-  const noticeColumns = [
-    {
-      title: "이름",
-      dataIndex: "name",
-      key: "name",
-      render: (text) => <span>{text}</span>,
-    },
-  ];
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    const fetchRSS = async () => {
+      try {
+        const response = await axios.get(
+          "https://apis.uiharu.dev/fixcors/api.php?url=https://www.korea.kr/rss/policy.xml"
+        );
+        parseString(response.data, (err, result) => {
+          const newsArray = result.rss.channel[0].item
+            .slice(0, 5)
+            .map((newsItem) => ({
+              title: newsItem.title[0],
+              link: newsItem.link[0],
+              pubDate: newsItem.pubDate[0]
+            }));
+          setNewsData(newsArray);
+        });
+      } catch (error) {
+        console.error("Error fetching RSS feed", error);
+      }
+    };
+    fetchRSS();
+  }, []);
+
+  const rows = [
+    createNotice("공지사항1"),
+    createNotice("공지사항2"),
+    createNotice("공지사항3"),
+    createNotice("공지사항4"),
+    createNotice("공지사항5")
+  ].slice(0, 6);
 
   const newsColumns = [
     {
       title: "뉴스 제목",
       dataIndex: "title",
       key: "title",
-      render: (text) => <span>{text}</span>,
+      render: (text, record) => (
+        <a
+          href={record.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={record.link}
+        >
+          {text}
+        </a>
+      )
     },
+    {
+      title: "날짜",
+      dataIndex: "pubDate",
+      key: "pubDate",
+      render: (text, record) => (
+        <span key={record.pubDate}>{new Date(text).toLocaleDateString()}</span>
+      )
+    }
   ];
 
   return (
     <>
-      <NavBar />
       <Layout style={{ padding: 0, margin: 0 }}>
         <Content>
           <div className="responsive-container">
-            <Card style={{ marginBottom: "20px", border: "none" }}>
-              <Title level={2}>공지사항</Title>
-              <Divider />
-              <Table
-                dataSource={rows}
-                columns={noticeColumns}
-                pagination={false}
-                rowClassName="noticeRow"
-              />
-            </Card>
-
             <Card style={{ border: "none" }}>
-              <Title level={2}>최신뉴스</Title>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                <Title level={2}>최신뉴스</Title>
+                <a
+                  href="https://www.korea.kr/main.do"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  더보기(제공: 대한민국 정책브리핑)
+                </a>
+              </div>
               <Divider />
               <Table
-                dataSource={news}
+                dataSource={newsData}
                 columns={newsColumns}
                 pagination={false}
                 rowClassName="newsRow"
+                rowKey={(record) => record.link}
               />
             </Card>
           </div>
         </Content>
 
-        <style jsx global>{`
-          
+        <style>{`
+          // 여기에 필요한 글로벌 스타일을 추가하세요
         `}</style>
       </Layout>
     </>

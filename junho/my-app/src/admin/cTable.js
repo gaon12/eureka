43f03088737a06aa5debe2cf74249fe6f297301d
dong0424 <54524618+dong0424@users.noreply.github.com>
2023-09-13 16:37:47 +0900

    코드

---
+++ junho/my-app/src/admin/cTable.js
@@ -0,0 +1,50 @@
+import { Table } from "antd";
+export default function CTable(props) {
+  const { columns, data } = props;
+  const getCategoryName = (categoryNumber) => {
+    switch (categoryNumber) {
+      case 1:
+        return "공지";
+      case 2:
+        return "이벤트";
+      case 3:
+        return "업데이트";
+      default:
+        return "기타";
+    }
+  };
+  const transformData = data.map((item) => {
+    if (item.hasOwnProperty("noti_category")) {
+      return {
+        ...item,
+        noti_category: getCategoryName(item.noti_category),
+      };
+    } else if (
+      item.hasOwnProperty("guest_car") &&
+      item.hasOwnProperty("electric_car") &&
+      item.hasOwnProperty("disabled_car")
+    ) {
+      return {
+        ...item,
+        guest_car: item.guest_car ? "True" : "False",
+        electric_car: item.electric_car ? "True" : "False",
+        disabled_car: item.disabled_car ? "True" : "False",
+      };
+    }
+    return item;
+  });
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+  const visibleData = transformData.slice(Math.max(transformData.length-5, 0)).reverse();
+  console.log(visibleData);
+  return (
+    <Table
+      columns={columns}
+      dataSource={visibleData}
+      onChange={onChange}
+      pagination={false}
+      rowKey={(record) => record.id}
+    ></Table>
+  );
+}
---
+++ junho/my-app/src/user/useNewsData.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { parseString } from "xml2js";
+
+export function useNewsData() {
+    const [newsData, setNewsData] = useState([]);
+  
+    useEffect(() => {
+      const fetchRSS = async () => {
+        try {
+          const response = await axios.get(
+            "https://apis.uiharu.dev/fixcors/api.php?url=https://www.korea.kr/rss/policy.xml"
+          );
+          parseString(response.data, (err, result) => {
+            const newsArray = result.rss.channel[0].item.map((newsItem) => ({
+              title: newsItem.title[0],
+              link: newsItem.link[0],
+              pubDate: newsItem.pubDate[0],
+            }));
+            setNewsData(newsArray);
+          });
+        } catch (error) {
+          console.error("Error fetching RSS feed", error);
+        }
+      };
+  
+      fetchRSS();
+    }, []);
+  
+    return newsData;
+  }
+  
+  
